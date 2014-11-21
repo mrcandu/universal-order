b1898d39251fb4036b06e4dab823746eb859c023
matthew_cohen <matthew_cohen@tvha.co.uk>
2014-11-21 17:17:04 +0000

    More CSS tidying

--- wp-content/themes/universal-order/single.php
+++ wp-content/themes/universal-order/single.php
@@ -3,16 +3,19 @@
       <section class="main">
                
         <?php while ( have_posts() ) : the_post(); ?>
+        
+        <h1 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <time datetime="<?php echo get_the_date(); ?>" title="<?php echo get_the_date(); ?>">
+          <?php echo get_the_date(); ?>
+        </time>
+
         <article>
-          <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-          <time datetime="<?php echo get_the_date(); ?>" title="<?php echo get_the_date(); ?>">
-            <?php echo get_the_date(); ?>
-          </time>
           <section>
             <?php the_content(); ?>
             <p class="read"><a href="<?php the_permalink(); ?>">Read more</a></p>
           </section>
         </article>
+        
         <?php endwhile; ?> 
         
       </section>
